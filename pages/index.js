02d7fa4525d03eb51bb3da5a7b61960e9702112c
Luca Cecchetti <luca.cecchetti07@gmail.com>
2020-10-22 01:55:37 +0200

    add intro

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,16 @@ import quotes from "lib/quotes";
 export default function Home() {
 
   // a monkey
-  var monkey = new Monkey(quotes, 1500, .005);
+  var monkey = new Monkey(quotes, 1000, .005);
 
   return (
     <div className={styles.home}>
       <div className={styles.header}>
         <h1 className={styles.logo}>🐒 Infinite Monkey Corp ©</h1>
-        <p></p>
+      </div>
+      <div className={styles.intro}>
+        <p>"The infinite monkey theorem states that a monkey hitting keys at random on a typewriter keyboard for an infinite amount of time will almost surely type any given text, such as the complete works of William Shakespeare"</p>
+        <p>By executing the Program, Infinite Monkey Corp will make available your private Digital MONKEY, which will be fed with Digital BANANAS each time it'll match a quote from the HAMLET.</p>
       </div>
       <MonkeyOutput monkey={monkey}/>
     </div>
