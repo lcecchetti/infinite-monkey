8c0a6fe632f8804d362bbebce9d4e451f35b2e3f
Luca Cecchetti <luca.cecchetti07@gmail.com>
2020-11-09 03:01:00 +0100

    refactor monkey lib as hook using a reducer to manage its own state

--- components/MonkeyOutput/index.js
+++ components/MonkeyOutput/index.js
@@ -1,67 +1,52 @@
 import { useState, useContext } from 'react';
 import IsOnContext from "lib/IsOnContext";
+import { useMonkey } from "lib/monkey";
 import styles from 'styles/components/MonkeyOutput.module.scss';
 
-const MonkeyOutput = ({ monkey }) => {
+const MonkeyOutput = ({ quotes, maxEssayLength, literateRatio }) => {
   const isOn = useContext(IsOnContext);
-  const [monkeyEssay, setMonkeyEssay] = useState(monkey.essay);
-  const [isAwake, setIsAwake] = useState(monkey.isAwake);
-  const [hasMoral, setHasMoral] = useState(false);
-
-  /**
-   * Force the monkey to the next action
-   * - No real monkeys were harmed during the making of this project -
-   */
-  const whipTheMonkey = () => {
-      // monkey's next action
-      monkey.do();
-
-      // update view status
-      setMonkeyEssay([...monkey.essay]);
-  };
+  const { monkey, awake, asleep } = useMonkey(quotes, maxEssayLength, literateRatio);
 
   /**
    * Toogle monkey isAwake
    */
   const toggleIsAwake = () => {
-    setHasMoral(monkey.hasQuoted);
-
     if (!monkey.isAwake) {
-      monkey.awake();
+      awake();
     }
     else {
-      monkey.asleep();
+      asleep();
     }
-
-    setIsAwake(monkey.isAwake);
   };
 
-  if (isAwake && isOn) {
-    setTimeout(() => {
-      whipTheMonkey();
-    }, monkey.speed);
+  // stop the monkey if monitor is off
+  if (!isOn && monkey.isAwake) {
+    asleep();
   }
 
   return (
-    <div className={`${styles.monkeyOutput} ${isAwake ? styles.awake : styles.asleep}`}>
+    <div className={`${styles.monkeyOutput} ${monkey.isAwake ? styles.awake : styles.asleep}`}>
+
       <div className={styles.actions}>
-        <button className={styles.awakeButton} onClick={toggleIsAwake}>{isAwake ? 'Stop The Monkey' : 'Execute Monkey Program'}</button>
+        <button className={styles.awakeButton} onClick={toggleIsAwake}>{monkey.isAwake ? 'Stop The Monkey' : 'Execute Monkey Program'}</button>
       </div>
-      {isAwake &&
+
+      {monkey.isAwake &&
         <div className={styles.monkeyEssay}>
-          {monkeyEssay.map((char, index) => (
+          {monkey.essay.map((char, index) => (
             char.isQuote ? <span className={styles.highlight} key={index}>{char.value}</span> : char.value
           ))}
         </div>
       }
 
-      {!isAwake && hasMoral &&
+      {!monkey.isAwake && monkey.hasQuoted &&
         <div className={styles.moral}>
           <p>Are you SURPRISED? You didn't think that the monkey would have written anything meaningful, didn't you?</p>
           <p>But what is truly suprising?</p>
           <p>The fact that the MONKEY did quote the hamlet? Or the fact that this branch of the UNIVERSE wrote down this program? Or that YOU are actually here, in front of the screen, reading a MONKEY quoting the HAMLET?</p>
         </div>
       }
+
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,18 @@
 import styles from 'styles/pages/Home.module.scss';
 import MonkeyOutput from "components/MonkeyOutput";
-import Monkey from 'lib/monkey';
 import quotes from "lib/quotes";
 
 export default function Home() {
-
-  // a monkey
-  var monkey = new Monkey(quotes, 2000, .001);
-
   return (
     <div className={styles.home}>
       <div className={styles.header}>
-        <h1 className={styles.logo}>🐒 Infinite Monkey Corp ©</h1>
+        <h1 className={styles.logo}>Infinite Monkey LAB</h1>
       </div>
       <div className={styles.intro}>
         <p>"The infinite monkey theorem states that a monkey hitting keys at random on a typewriter keyboard for an infinite amount of time will almost surely type any given text, such as the complete works of William Shakespeare"</p>
-        <p>By executing the Program, Infinite Monkey Corp will make available your private Digital MONKEY, which will be fed with Digital BANANAS each time it'll match a quote from the HAMLET.</p>
+        <p>By executing the Program, Infinite Monkey LAB provides your own Digital MONKEY instance, which will be fed with Digital BANANAS each time it'll match a quote from the HAMLET.</p>
       </div>
-      <MonkeyOutput monkey={monkey}/>
+      <MonkeyOutput quotes={quotes} maxEssayLength={2000} literateRatio={0.001} />
     </div>
   );
 }
