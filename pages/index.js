8aa3c262f209d3b1ba46b7c41eb85d5761470fb4
Luca Cecchetti <luca.cecchetti07@gmail.com>
2020-11-15 02:11:01 +0100

    add sound effect

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import styles from 'styles/pages/Home.module.scss';
 import MonkeyOutput from "components/MonkeyOutput";
 import quotes from "lib/quotes";
 
-export default function Home() {
+const Home = () => {
   return (
     <div className={styles.home}>
       <div className={styles.header}>
@@ -12,7 +12,9 @@ export default function Home() {
         <p>"The infinite monkey theorem states that a monkey hitting keys at random on a typewriter keyboard for an infinite amount of time will almost surely type any given text, such as the complete works of William Shakespeare"</p>
         <p>By clicking execute, Infinite Monkey LAB will provide you a Digital MONKEY, a Virtual KEYBOARD and an infinite amount of TIME.</p>
       </div>
-      <MonkeyOutput quotes={quotes} maxEssayLength={1000} literateRatio={0.001} />
+      <MonkeyOutput quotes={quotes} maxEssayLength={1000} literateRatio={0.0005} />
     </div>
   );
 }
+
+export default Home;
