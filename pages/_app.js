2428ddb00ef6dc51b01408b204074ba2b863f517
Luca Cecchetti <luca.cecchetti07@gmail.com>
2020-11-15 21:22:36 +0100

    add head metas

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,24 @@ function InfiniteMonkeyApp({Component, pageProps}) {
   return (
     <>
       <Head>
-        <title>Infinite Monkey Corp</title>
+        <title>Infinite Monkey Lab</title>
+        <meta name="description" content="The infinite monkey theorem states that a monkey hitting keys at random on a typewriter keyboard for an infinite amount of time will almost surely type any given text, such as the complete works of William Shakespeare"/>
+        <meta
+          key="og:type"
+          name="og:type"
+          content="website"
+        />
+        <meta
+          key="og:title"
+          name="og:title"
+          content="Infinite Monkey Lab"
+        />
+        <meta
+          key="og:description"
+          name="og:description"
+          content="The infinite monkey theorem states that a monkey hitting keys at random on a typewriter keyboard for an infinite amount of time will almost surely type any given text, such as the complete works of William Shakespeare"
+        />
+
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
       </Head>
 
