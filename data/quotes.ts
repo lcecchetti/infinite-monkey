23df68cca91b70219ec28165b669b44dc2c6799c
Luca Cecchetti <luca.cecchetti07@gmail.com>
2026-08-01 20:35:25 +0100

    add more quotes

--- data/quotes.ts
+++ data/quotes.ts
@@ -81,6 +81,96 @@ const quotes: Quote[] = [
     author: 'Ernest Hemingway',
     work: 'The Old Man and the Sea',
   },
+  {
+    quote: 'It was the best of times it was the worst of times',
+    author: 'Charles Dickens',
+    work: 'A Tale of Two Cities',
+  },
+  {
+    quote: 'Call me Ishmael',
+    author: 'Herman Melville',
+    work: 'Moby-Dick',
+  },
+  {
+    quote: 'All animals are equal but some animals are more equal than others',
+    author: 'George Orwell',
+    work: 'Animal Farm',
+  },
+  {
+    quote: 'Two roads diverged in a yellow wood and sorry I could not travel both',
+    author: 'Robert Frost',
+    work: 'The Road Not Taken',
+  },
+  {
+    quote: 'I think therefore I am',
+    author: 'René Descartes',
+    work: 'Discourse on the Method',
+  },
+  {
+    quote: 'The unexamined life is not worth living',
+    author: 'Socrates',
+    work: 'Apology, by Plato',
+  },
+  {
+    quote: 'Curiouser and curiouser cried Alice',
+    author: 'Lewis Carroll',
+    work: 'Alice\'s Adventures in Wonderland',
+  },
+  {
+    quote: 'Ask not what your country can do for you ask what you can do for your country',
+    author: 'John F Kennedy',
+    work: 'Inaugural Address, 1961',
+  },
+  {
+    quote: 'I have a dream that one day this nation will rise up and live out the true meaning of its creed',
+    author: 'Martin Luther King Jr',
+    work: 'I Have a Dream, 1963',
+  },
+  {
+    quote: 'The only thing we have to fear is fear itself',
+    author: 'Franklin D Roosevelt',
+    work: 'First Inaugural Address, 1933',
+  },
+  {
+    quote: 'In the room the women come and go talking of Michelangelo',
+    author: 'T S Eliot',
+    work: 'The Love Song of J Alfred Prufrock',
+  },
+  {
+    quote: 'Do not go gentle into that good night old age should burn and rave at close of day',
+    author: 'Dylan Thomas',
+    work: 'Do Not Go Gentle into That Good Night',
+  },
+  {
+    quote: 'Friends Romans countrymen lend me your ears',
+    author: 'William Shakespeare',
+    work: 'Julius Caesar',
+  },
+  {
+    quote: 'Sing to me of the man Muse the man of twists and turns',
+    author: 'Homer',
+    work: 'The Odyssey',
+  },
+  {
+    quote: 'In a village of La Mancha the name of which I have no desire to recall',
+    author: 'Miguel de Cervantes',
+    work: 'Don Quixote',
+  },
+  {
+    quote: 'Mother died today or maybe yesterday I cant remember',
+    author: 'Albert Camus',
+    work: 'The Stranger',
+  },
+  {
+    quote: 'Many years later as he faced the firing squad Colonel Aureliano Buendia was to remember that distant afternoon when his father took him to discover ice',
+    author: 'Gabriel García Márquez',
+    work: 'One Hundred Years of Solitude',
+  },
+  {
+    quote: 'Mrs Dalloway said she would buy the flowers herself',
+    author: 'Virginia Woolf',
+    work: 'Mrs Dalloway',
+  },
 ];
 
 export default quotes;
